87fe2ab3b63f4d7d5af01d39a53c48ada7e09092
William Jiang <wj2065@nyu.edu>
2025-03-23 20:30:52 -0400

    new

---
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const HeroSection = () => {
+  return (
+    <div className="relative h-screen">
+      <Image
+        src="/landing-splash.jpg"
+        alt="Rentiful Rental Platform Hero Section"
+        fill
+        className="object-cover object-center"
+        priority
+      />
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/60" />  
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-full"
+      >
+        <div className="max-w-4xl mx-auto px-16 sm:px-12">
+          <h1 className="text-5xl font-bold text-white mb-4">
+            Start your journey to finding the perfect place to call home.
+          </h1>
+          <p className="text-xl text-white mb-8 ">
+            Explore our wide range of properties to fit your lifestyle and
+            needs!
+          </p>
+          <div className="flex justify-center">
+            <Input
+              type="text"
+              value="search query"
+              onChange={() => {}}
+              placeholder="Search by city, neighborhood or address"
+              className="w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12"
+            />
+            <Button
+              onClick={() => {}}
+              className="bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12"
+            >
+              Search
+            </Button>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default HeroSection;
