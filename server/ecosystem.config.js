9df7ff7f7483c054a987539f740896afa7447c61
William Jiang <wj2065@nyu.edu>
2025-04-29 10:21:36 -0400

    ecosystemconfig name

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "inventory-management",
+      name: "real-estate",
       script: "npm",
       args: "run dev",
       env: { NODE_ENV: "development", },
